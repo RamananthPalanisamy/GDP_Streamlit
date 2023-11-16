d2eda19856d07880259de6bb313ca47b6c602cdf
P <Ramananth.P@cbre.com>
2023-11-16 15:03:28 +0530

    latest_changes

--- login.py
+++ login.py
@@ -3,6 +3,7 @@ from PIL import Image           #Used to display the CBRE UK Logo
 from datetime import datetime   #Used for datetime functions
 import snowflake.connector      #Used for Snowflake Connectivity
 from snowflake.snowpark import Session
+
 #Setup streamlit to use wide layout
 st.set_page_config(page_title="Streamlit Visualization Demo", page_icon="CBRE_UK.jpg", layout="wide")
 # Create an empty container
@@ -18,7 +19,7 @@ with placeholder.form("login"):
     password = st.text_input("Password", type="password")
     submit = st.form_submit_button("Login")
 
-if submit and email == actual_email and password == actual_password:
+if submit and email != '' and password == actual_password:
     # If the form is submitted and the email and password are correct,
     # clear the form/container and display a success message
     placeholder.empty()
